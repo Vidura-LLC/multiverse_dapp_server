e52c97b5a3cbf1c9129b568d0921c20614e89ed1
Hamad Ullah <hamadullah16@gmail.com>
2025-02-04 18:18:43 +0500

    attempting to deploy the server on vercel

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,37 @@
-import { Request, Response } from "express";
-import app from "./app";
+import express, { Response, Request, Application } from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import stakingRoutes from "./staking/stakingRoutes";
 
 // Load environment variables
 dotenv.config();
+
+const app: Application = express();
+
+// Middleware
+app.use(cors({
+    origin: '*', // Allow frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true // Allow cookies and authentication headers
+}));
+
+
+app.use(bodyParser.json());
+
+// Routes
+app.use("/api/staking", stakingRoutes);
 
 // Server Port
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req: Request, res: Response): any => {
-  return res.json({ message: "Server Up" });
+    return res.json({ message: "Server Up" });
 })
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+export default app;
