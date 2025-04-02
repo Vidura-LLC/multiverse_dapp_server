81f6b6a39326ca23611a36e979675ea0178dff25
sickbeoi <saadatullah21@gmail.com>
2025-04-03 04:33:51 +0500

    Added leaderboard functionality for tournaments

--- src/gamehub/gamehubRoutes.ts
+++ src/gamehub/gamehubRoutes.ts
@@ -44,7 +44,7 @@ router.get('/tournament-leaderboard/:id', getTournamentLeaderboardController as 
 router.post('/score/update', verifyUser as unknown as RequestHandler, updateParticipantScoreController as unknown as RequestHandler);
 
 // Route to get tournaments by game
-router.get('/tournaments/game/:gameId', getTournamentsByGameController as unknown as RequestHandler);
+router.get('/get-tournament-by-game/:gameId', getTournamentsByGameController as unknown as RequestHandler);
 
 
 
